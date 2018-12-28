15e8d62fd8e9e169abc3812c701fb8c6e72a2492
Andres Mena <ballerlikemike23@gmail.com>
2018-12-27 21:33:07 -0500

    Function to Find Note

--- notes.js
+++ notes.js
@@ -1,4 +1,4 @@
-const notes = [{}, {
+const notes = [{
   title: 'My next trip',
   body: 'I would like to go to Spain'
 }, {
@@ -9,26 +9,21 @@ const notes = [{}, {
   body: 'Get a new seat'
 }]
 
-// console.log(notes.pop())
-// notes.push('My new note')
+const findNote = function (notes, noteTitle) {
+  const index = notes.findIndex(function (note) {
+    return note.title.toLowerCase() === noteTitle.toLowerCase()
+  })
+  return notes[index]
+}
 
-// console.log(notes.shift())
-// notes.unshift('My first note')
+const note = findNote(notes, 'office modification')
+console.log(note)
 
-// notes.splice(1, 1, 'This is the new second item')
+// console.log(notes.length)
+// console.log(notes)
 
-// notes[2] = 'This is now the new note 3'
-
-// notes.forEach(function (item, index) {
-//   console.log(index)
-//   console.log(item)
+// const index = notes.findIndex(function (note, index) {
+//   console.log(note)
+//   return note.title === 'Habits to work on'
 // })
-
-console.log(notes.length)
-console.log(notes)
-
-const index = notes.findIndex(function (note, index) {
-  console.log(note)
-  return note.title === 'Habits to work on'
-})
-console.log(index)
+// console.log(index)
