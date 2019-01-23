3de36a0e1b95b84b222e7096072835fe34eee9e8
Andres Mena <ballerlikemike23@gmail.com>
2019-01-22 21:10:25 -0500

    More refactoring and some new functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import { getNotes, createNote } from './notes'
+import { getNotes, createNote, removeNote, updateNote } from './notes'
+import { getFilters, setFilters } from './filters'
 
-console.log(getNotes())
-createNote()
-console.log(getNotes())
+// console.log(getNotes())
+// createNote()
+// // removeNote()
+// // updateNote()
+// console.log(getNotes())
+
+console.log(getFilters())
+setFilters({
+  searchText: 'Office',
+  sortBy: 'byCreated'
+})
+console.log(getFilters())
